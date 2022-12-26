c2071884e61ade812e2418f87a15fb37540761c1
Saket Kothari <saketkothari722@gmail.com>
2022-12-26 23:08:58 +0530

    🎉 Next.js App with tailwind config

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+      hello
+    </div>
+  );
+}
