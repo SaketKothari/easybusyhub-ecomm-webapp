b2b6be9a17831a5438aa0c78ff4d461601be2bb7
Saket Kothari <saketkothari722@gmail.com>
2022-12-27 11:25:16 +0530

    Building the Right Side of the Top Nav Section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,10 +11,10 @@ function Header() {
   const router = useRouter();
 
   return (
-    <header>
+    <header className="sticky top-0 z-50">
       {/* Top Nav */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
-        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+        <div className="mr-5 mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src={amazonLogo}
             onClick={() => router.push('/')}
@@ -39,6 +39,29 @@ function Header() {
             }
           />
           <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/* Right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link cursor-pointer">
+            <p className="hover:underline">Sign in</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span
+              className={`absolute top-0 right-0 md:right-10 h-4  bg-yellow-400 text-center rounded-full text-black font-bold`}
+            >
+              1
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
         </div>
       </div>
     </header>
