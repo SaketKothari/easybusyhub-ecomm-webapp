e6322af9e016453fa541c6a1fc3208c820410654
Saket Kothari <saketkothari722@gmail.com>
2022-12-27 10:47:17 +0530

    Top Nav Section of the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,17 @@ function Header() {
   return (
     <header>
       {/* Top Nav */}
-      <div>
-        <div>
-          <Image src={amazonLogo} width={150} height={40} alt="Amazon-Logo" />
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src={amazonLogo}
+            onClick={() => router.push('/')}
+            width={150}
+            height={40}
+            alt="Amazon-Logo"
+            objectFit="contain"
+            className="cursor-pointer active:transform active:scale-90"
+          />
         </div>
       </div>
     </header>
