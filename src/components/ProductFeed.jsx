94ac302d21975b615d95ff95bde0c666cbe2f26b
Saket Kothari <saketkothari722@gmail.com>
2022-12-28 12:53:11 +0530

    Building the Product Component which is inside the ProductFeed Component

--- src/components/ProductFeed.jsx
+++ src/components/ProductFeed.jsx
@@ -1,9 +1,18 @@
+import Product from './Product';
+
 function ProductFeed({ products }) {
   return (
     <div>
-      <h1>Products</h1>
-      {products.map((product) => (
-        <p>{product.title}</p>
+      {products.map(({ id, title, price, description, category, image }) => (
+        <Product
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
       ))}
     </div>
   );
