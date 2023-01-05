942766e984c070cf75273974f31ce34ece90b13c
Saket Kothari <saketkothari722@gmail.com>
2023-01-05 21:24:42 +0530

    Setting Up & Implementing Firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBDwLexb2VEViprRAXBVQYi1x1wgPJ9o3E',
+  authDomain: 'amzn-2-yt-58840.firebaseapp.com',
+  projectId: 'amzn-2-yt-58840',
+  storageBucket: 'amzn-2-yt-58840.appspot.com',
+  messagingSenderId: '117936636660',
+  appId: '1:117936636660:web:3d3ed367fe52ced5ed94a1',
+};
