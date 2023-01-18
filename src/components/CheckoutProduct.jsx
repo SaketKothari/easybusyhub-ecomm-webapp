990727bef47ea41d35dbcc589c2b9a2c8dc51f37
Saket Kothari <saketkothari722@gmail.com>
2023-01-18 10:26:38 +0530

    Creating the Checkout Product Component

---
+++ src/components/CheckoutProduct.jsx
@@ -0,0 +1,15 @@
+function CheckoutProduct({
+  id,
+  title,
+  price,
+  rating,
+  description,
+  category,
+  image,
+  hasPrime,
+  quantity,
+}) {
+  return <div>CheckoutProduct</div>;
+}
+
+export default CheckoutProduct;
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 import Header from '../components/Header';
+import CheckoutProduct from '../components/CheckoutProduct';
+import { useSelector } from 'react-redux';
+import { selectItems } from '../slices/basketSlice';
 
 function Checkout() {
+  const items = useSelector(selectItems);
+
   return (
     <div className="bg-gray-100">
       <Header />
@@ -18,7 +23,30 @@ function Checkout() {
         </div>
 
         <div className="flex flex-col p-5 space-y-50 bg-white">
-          <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+          <h1
+            className={`text-3xl ${
+              items.length > 0 ? 'border-b pb-4' : 'pb-2'
+            }`}
+          >
+            {items.length === 0
+              ? 'Your Amazon Basket is empty.'
+              : 'Shopping Basket'}
+          </h1>
+
+          {items.map((item, i) => (
+            <CheckoutProduct
+              key={i}
+              id={item.id}
+              title={item.title}
+              rating={item.rating}
+              price={item.price}
+              description={item.description}
+              category={item.category}
+              image={item.image}
+              hasPrime={item.hasPrime}
+              quantity={item.length}
+            />
+          ))}
         </div>
 
         {/* Right */}
