224eaef2946ac4181e535ce206caaaa731bda0c1
Saket Kothari <saketkothari722@gmail.com>
2023-01-18 11:09:56 +0530

    Implementing Add & Remove Item Functionality

--- src/components/CheckoutProduct.jsx
+++ src/components/CheckoutProduct.jsx
@@ -1,15 +1,91 @@
-function CheckoutProduct({
-  id,
-  title,
-  price,
-  rating,
-  description,
-  category,
-  image,
-  hasPrime,
-  quantity,
-}) {
-  return <div>CheckoutProduct</div>;
+import { MinusSmIcon, PlusIcon, StarIcon } from '@heroicons/react/solid';
+import Currency from 'react-currency-formatter';
+import Image from 'next/image';
+import { useDispatch } from 'react-redux';
+import { addToBasket, removeFromBasket } from '../slices/basketSlice';
+
+function CheckoutProduct(props) {
+  const dispatch = useDispatch();
+  const id = props.id;
+  const title = props.title;
+  const rating = props.rating;
+  const price = props.price;
+  const description = props.description;
+  const category = props.category;
+  const image = props.image;
+  const hasPrime = props.hasPrime;
+  const quantity = props.quantity;
+
+  function addItemToBasket() {
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+      rating,
+      hasPrime,
+    };
+
+    // Push item into redux
+    dispatch(addToBasket(product));
+  }
+
+  function removeItemFromBasket() {
+    dispatch(removeFromBasket({ id }));
+  }
+
+  return (
+    <div className="block py-4 sm:grid sm:grid-cols-5 my-16 sm:my-3">
+      <div className="text-center sm:text-left">
+        <Image src={image} width={200} height={200} objectFit="contain" />
+      </div>
+
+      {/* Middle */}
+      <div className="col-span-3 mx-5 mb-4 sm:mb-0">
+        <p className="my-3">{title}</p>
+        <div className="flex">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <StarIcon key={i} className="h-5 text-yellow-500" />
+            ))}
+        </div>
+        <p className="text-xs my-2 line-clamp-3">{description}</p>
+        <span className="font-bold">
+          <Currency quantity={price * 71} currency="INR" />
+        </span>
+        {hasPrime && (
+          <div className="flex items-center space-x-2">
+            <img
+              loading="lazy"
+              className="w-12"
+              src="https://links.papareact.com/fdw"
+              alt=""
+            />
+            <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+          </div>
+        )}
+      </div>
+
+      {/* Buttons on the right of the products */}
+      <div className="flex flex-col space-y-2 my-auto justify-self-end">
+        <div className="flex justify-between xs:justify-start">
+          <button className="button sm:p-1" onClick={removeItemFromBasket}>
+            <MinusSmIcon className="h-5 text-black" />
+          </button>
+          <div className="p-2 whitespace-normal sm:p-1 sm:whitespace-nowrap">
+            Quantity: <span className="font-bold">{quantity}</span>
+          </div>
+          <button className="button sm:p-1" onClick={addItemToBasket}>
+            <PlusIcon className="h-5 text-black" />
+          </button>
+        </div>
+        <button className="button">Remove from Basket</button>
+      </div>
+    </div>
+  );
 }
 
 export default CheckoutProduct;
--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -11,7 +11,20 @@ export const basketSlice = createSlice({
     addToBasket: (state, action) => {
       state.items = [...state.items, action.payload];
     },
-    removeFromBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {
+      let pos = state.items.findIndex((item) => item.id === action.payload.id);
+      let newBasket = [...state.items];
+
+      if (pos > -1) {
+        newBasket.splice(pos, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.payload.id}) as its not in the basket`
+        );
+      }
+
+      state.items = newBasket;
+    },
   },
 });
 
