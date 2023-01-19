3cc97f639bec388b09f18fff76eb65ce313ab12f
Saket Kothari <saketkothari722@gmail.com>
2023-01-19 15:34:28 +0530

    Clears the basket when order is complete on success page

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -8,6 +8,9 @@ export const basketSlice = createSlice({
   name: 'basket',
   initialState,
   reducers: {
+    hydrate: (state, action) => {
+      return action.payload;
+    },
     addToBasket: (state, action) => {
       state.items = [...state.items, action.payload];
     },
@@ -32,11 +35,19 @@ export const basketSlice = createSlice({
 
       state.items = newBasket;
     },
+    clearBasket: (state, action) => {
+      state.items = [];
+    },
   },
 });
 
-export const { addToBasket, removeFromBasket, removeGroupedFromBasket } =
-  basketSlice.actions;
+export const {
+  addToBasket,
+  removeFromBasket,
+  removeGroupedFromBasket,
+  clearBasket,
+  hydrate,
+} = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
 export const selectItems = (state) => state.basket.items;
